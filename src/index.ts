47a44125a5faa50a66eef389dd5181cbbc5dc1ed
David Katz <dhkatz.16@gmail.com>
2019-04-06 13:45:31 -0700

    Testing alternate error solution

--- src/index.ts
+++ src/index.ts
@@ -12,28 +12,31 @@ export interface Options {
 export default function(options?: Options) {
   const patterns = options && options.exclude ? options.exclude.map((value: string) => new RegExp(value)) : [];
 
-  return async function headers(ctx: koa.BaseContext, next) {
+  return async function headers(ctx: koa.Context, next) {
     const exclude = ctx.query.jsonapiexclude || false;
 
     if (exclude) {
-      return next();
+      return await next();
     }
 
     if (patterns.some((pattern: RegExp) => pattern.test(ctx.url))) {
-      return next();
+      return await next();
     }
 
     if (!ctx.header.accept || !/application\/vnd\.api\+json/.test(ctx.header.accept)) {
-      ctx.throw(400, {
-        message: {
-          errors: [
-            {
-              code: 'invalid_request',
-              title: 'API requires header "Accept application/vnd.api+json" for exchanging data.',
-            },
-          ],
-        },
-      });
+      ctx.status = 400;
+      ctx.type = 'application/vnd.api+json';
+      ctx.body = {
+        errors: [
+          {
+            code: 'invalid_request',
+            status: 400,
+            title: 'API requires header "Accept application/vnd.api+json" for exchanging data.',
+          },
+        ],
+      };
+
+      ctx.app.emit('error', 'API requires header "Accept application/vnd.api+json" for exchanging data.', ctx);
     }
 
     // Content-type: application/vnd.api+json
