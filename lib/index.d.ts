0f8e4807f94db9b1171b7c70dac76e03a67bd817
David Katz <dhkatz.16@gmail.com>
2019-04-06 13:47:40 -0700

    Update missing lib

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -6,4 +6,4 @@ export interface Options {
  * Add JSON API compliant headers to Koa.
  * @param options Customization options for handling requests.
  */
-export default function (options?: Options): (ctx: koa.BaseContext, next: any) => Promise<any>;
+export default function (options?: Options): (ctx: koa.Context, next: any) => Promise<any>;
